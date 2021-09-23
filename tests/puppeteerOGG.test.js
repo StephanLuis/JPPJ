31f1b0ffab76fe836ff8d1b0f605fbbd0e8e618f
StephanLuis <stephan@learnsense.org>
2021-09-23 21:14:39 +0000

    getting integration tests ready

---
+++ tests/puppeteerOGG.test.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+(async () => {
+const browser = await puppeteer.launch();
+
+const page = await browser.newPage();
+await page.goto('http://practisemaster.com/demos/puppeteerEmpty.html');
+
+console.log(await page.content());
+
+await page.evaluate(() => {
+  let dom = document.querySelector('#EmptyDiv');
+
+
+
+  var video = document.createElement("video");
+  video.id = 'player';
+  video.innerHTML = '<source src="https://www.w3schools.com/html/mov_bbb.ogg" type="video/ogg">'
+
+  dom.append(video);
+
+});
+
+await page.waitFor(5000);
+
+await page.screenshot({ path: 'screenshot.png' });
+
+await browser.close();
+
+})();
