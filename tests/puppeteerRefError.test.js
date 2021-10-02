c4fb7c4150b85767a06b388f58fab284a281cf88
StephanLuis <stephan@learnsense.org>
2021-10-02 21:02:40 +0000

    quick update

--- tests/puppeteerRefError.test.js
+++ tests/puppeteerRefError.test.js
@@ -11,14 +11,16 @@ const puppeteer = require('puppeteer');
   
   // page.on('requestfailed', request => { console.log(request.failure().errorText, request.url); }); 
   
-  page.on('requestfailed', request => {
-    console.log(`url: ${request.url()}, errText: ${request.failure().errorText}, method: ${request.method()}`)
-});
+//   page.on('requestfailed', request => {
+//     console.log(`url: ${request.url()}, errText: ${request.failure().errorText}, method: ${request.method()}`)
+// });
   
-  await page.goto('https://practisemaster.com/demos/puppeteerBrowserError5.html');
+  await page.goto('https://8000-fuchsia-leech-e3d1fb5t.ws-eu18.gitpod.io/dist/puppeteerPMogg.html');
   // await page.goto('http://practisemaster.com/demos/puppeteerBrowserError.html');
   
-  
+// try writing a positive conditional based on:
+  // https://www.tabnine.com/code/javascript/functions/puppeteer/Response/status
+  await page.waitForResponse(response => response.status() === 200)
   
   
   // await page.waitFor(5000); 
